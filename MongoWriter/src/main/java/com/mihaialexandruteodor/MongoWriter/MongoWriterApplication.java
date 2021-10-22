2958431d17b14536664e0343a0ad18145b2f2ef6
mihaiteodor97 <mihaialexandruteodor@gmail.com>
2021-10-22 13:26:09 +0300

    start.spring.io generated Spring Boot Application

---
+++ MongoWriter/src/main/java/com/mihaialexandruteodor/MongoWriter/MongoWriterApplication.java
@@ -0,0 +1,13 @@
+package com.mihaialexandruteodor.MongoWriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoWriterApplication.class, args);
+	}
+
+}
---
+++ MongoWriter/src/test/java/com/mihaialexandruteodor/MongoWriter/MongoWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihaialexandruteodor.MongoWriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
