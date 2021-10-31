9b7a4d311ee78b51c309cfbf8d62dc96ee95230c
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2021-10-31 14:12:33 +0200

    added new home template

--- MongoWriter/src/main/java/com/mihaialexandruteodor/FeatherWriter/controllers/HomeController.java
+++ MongoWriter/src/main/java/com/mihaialexandruteodor/FeatherWriter/controllers/HomeController.java
@@ -1,18 +1,14 @@
 package com.mihaialexandruteodor.FeatherWriter.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @Value("${spring.application.name}")
-    String appName;
-
-    @GetMapping("/")
-    public String homePage(Model model) {
-        model.addAttribute("appName", appName);
-        return "home";
+    @RequestMapping({"","/","/index"})
+    public String home(Model model)
+    {
+        return "index";
     }
 }
