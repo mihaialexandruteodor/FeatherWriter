4de6059560775685c7f35c7d13b8bf5238b7bcf6
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-01-20 12:00:12 +0200

    load image on ch page

--- src/main/java/com/mihaialexandruteodor/FeatherWriter/controllers/CharacterProfileController.java
+++ src/main/java/com/mihaialexandruteodor/FeatherWriter/controllers/CharacterProfileController.java
@@ -1,10 +1,14 @@
 package com.mihaialexandruteodor.FeatherWriter.controllers;
 
+import com.mihaialexandruteodor.FeatherWriter.model.FWCharacter;
 import com.mihaialexandruteodor.FeatherWriter.services.FWCharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class CharacterProfileController {
@@ -19,8 +23,10 @@ public class CharacterProfileController {
 
 
     @GetMapping("/characterProfile/{id}")
-    public ModelAndView characterProfile() {
+    public ModelAndView characterProfile(@Valid @PathVariable(value = "id") int id) {
+        FWCharacter fwcharacter = fwCharacterService.getFWCharacterById(id);
         ModelAndView mv = new ModelAndView("character_profile");
+        mv.addObject("fwcharacter", fwcharacter);
         return mv;
     }
 }
