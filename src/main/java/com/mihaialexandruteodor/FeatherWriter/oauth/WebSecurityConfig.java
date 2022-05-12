1ff13ebe2a85f717d1c5b42ecc4722c65ee897eb
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-05-12 15:20:50 +0300

    bg for login page

--- src/main/java/com/mihaialexandruteodor/FeatherWriter/oauth/WebSecurityConfig.java
+++ src/main/java/com/mihaialexandruteodor/FeatherWriter/oauth/WebSecurityConfig.java
@@ -49,6 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/login", "/oauth/**").permitAll()
+                .antMatchers("/js/**", "/css/**", "/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
